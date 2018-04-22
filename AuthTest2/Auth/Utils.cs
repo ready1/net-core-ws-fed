44c204fe29b21cdb4217c7b66a79e135f2a9eea3
ready1 <jvista@gmail.com>
2018-04-23 09:42:51 +1000

    initial commit

---
+++ AuthTest2/Auth/Utils.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using DimensionData.Toolset.ServiceDiscovery;
+using DimensionData.Toolset.Validation;
+
+namespace AuthTest2.Auth
+{
+    internal static class Utils
+    {
+        /// <summary>
+        ///     Get endpoint address from Service Discovery.
+        /// </summary>
+        /// <param name="serviceName">Name of service to discover.</param>
+        /// <param name="appKey">The application key of the endpoint.</param>
+        /// <param name="locationKey">The location key of the endpoint.</param>
+        /// <returns>The endpoint of the discovered service.</returns>
+        public static Uri GetServiceEndpointFromDnsConvention(string serviceName = null, string appKey = null,
+            string locationKey = null)
+        {
+            var formatter = new DnsServiceEndpointFormatter();
+            return formatter.Format(appKey, serviceName, locationKey);
+        }
+
+        /// <summary>
+        ///     Get a certificate from a certificate store by thumbprint.
+        /// </summary>
+        /// <param name="thumbprint">The thumbprint of the certificate.</param>
+        /// <param name="storeName">The name of the store to look for the certificate.</param>
+        /// <param name="storeLocation">The location of the store to look for the certificate.</param>
+        /// <returns> The matching <see cref="X509Certificate2" />, or <c>null</c> if no matching certificate found.</returns>
+        public static X509Certificate2 GetCertificateByThumbprint(string thumbprint, StoreName storeName = StoreName.My,
+            StoreLocation storeLocation = StoreLocation.LocalMachine)
+        {
+            Check.ArgumentNotNullOrWhiteSpace(thumbprint, nameof(thumbprint));
+
+            using (var store = new X509Store(storeName, storeLocation))
+            {
+                store.Open(OpenFlags.ReadOnly);
+                var certificates = store.Certificates.Find(X509FindType.FindByThumbprint, thumbprint, false);
+                if (certificates.Count == 0) return null;
+
+                return certificates[0];
+            }
+        }
+
+        /// <summary>
+        ///     Get a certificate from a base64 encoded string.
+        /// </summary>
+        /// <param name="encodedCert">The base64 encoded certificate.</param>
+        /// <returns> The decoded <see cref="X509Certificate2" />.</returns>
+        public static X509Certificate2 GetCertificateByEncodedString(string encodedCert)
+        {
+            Check.ArgumentNotNullOrWhiteSpace(encodedCert, nameof(encodedCert));
+
+            var rawCertificate = Convert.FromBase64String(encodedCert);
+            return new X509Certificate2(rawCertificate);
+        }
+    }
+}
---
+++ AuthTest2/Startup.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using System.Web;
+using AuthTest2.Auth;
+using DimensionData.Toolset.Configuration;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.WsFederation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.IdentityModel.Protocols.WsFederation;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Rest.Azure;
+
+namespace AuthTest2
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(sharedOptions =>
+            {
+                sharedOptions.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                sharedOptions.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                sharedOptions.DefaultChallengeScheme = WsFederationDefaults.AuthenticationScheme;
+            })
+            .AddWsFederation(options =>
+            {
+                options.Wtrealm = "https://cloud.dimensiondata.com/portal/v1/ci";
+                options.MetadataAddress = "https://adfs-dev.gmgmt.dimensiondata.com/FederationMetadata/2007-06/federationmetadata.xml";
+
+                options.Events = new WsFederationEvents
+                {
+                    OnRedirectToIdentityProvider = notifications =>
+                    {
+                        if (notifications.ProtocolMessage.IsSignInMessage)
+                        {
+                            // Read and set additional sign in parameters
+                            string signInParameters = Configuration["dimensionData:ui:sts:signInParameters"];
+                            if (!string.IsNullOrWhiteSpace(signInParameters))
+                            {
+                                signInParameters = HttpUtility.HtmlDecode(signInParameters);
+                                Debug.Assert(signInParameters != null, "signInParameters != null");
+                                string[] parameters = signInParameters.Split('&');
+                                foreach (string param in parameters)
+                                {
+                                    string[] keyValue = param.Split('=');
+                                    if (keyValue.Length > 0)
+                                    {
+                                        notifications.ProtocolMessage.Parameters.Add(keyValue[0], keyValue[1]);
+                                    }
+                                }
+                            }
+                        }
+                        return Task.CompletedTask;
+                    },
+                    OnSecurityTokenValidated = context =>
+                    {
+                        var i = 0;
+                        return Task.CompletedTask;
+                    },
+                    OnMessageReceived = context =>
+                    {
+                        var i = 0;
+                        return Task.CompletedTask;
+                    },
+                    OnTicketReceived = context =>
+                    {
+                        var i = 0;
+                        return Task.CompletedTask;
+                    }
+                };
+            })
+            .AddCookie();
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // TODO: Callbacks are not working
+        public void ConfigureServices2(IServiceCollection services)
+        {
+            var appKey = Configuration["dimensionData:ui:auth:appKey"];
+            if (appKey == null)
+            {
+                throw new Exception("Missing AppKey configuration.");
+            }
+
+            var stsConfig = Configuration["dimensionData:ui:sts:certificateThumbprint"];
+            if (stsConfig == null)
+            {
+                throw new Exception("Missing Ws-Federation authentication configuration.");
+            }
+
+            X509Certificate2 certificate;
+            var stsCertificateThumbprint = Configuration["dimensionData:ui:sts:certificateThumbprint"];
+            var stsCertificate = Configuration["dimensionData:ui:sts:certificate"];
+            var stsRealm = Configuration["dimensionData:ui:sts:realm"];
+            var stsReplyTo = Configuration["dimensionData:ui:sts:replyTo"];
+            var stsIssuer = Configuration["dimensionData:ui:sts:issuer"];
+            var stsSignInEndpoint = Configuration["dimensionData:ui:sts:signInEndpoint"];
+            var stsSignInPath = Configuration["dimensionData:ui:sts:signInPath"];
+            if (!string.IsNullOrEmpty(stsCertificateThumbprint))
+            {
+                certificate = Utils.GetCertificateByThumbprint(stsCertificateThumbprint);
+                if (certificate == null)
+                {
+                    throw new Exception($"Certificate with thumbprint '{stsCertificateThumbprint}' not found in local store.");
+                }
+            }
+            else if (!string.IsNullOrEmpty(stsCertificate))
+            {
+                certificate = Utils.GetCertificateByEncodedString(stsCertificate);
+            }
+            else
+            {
+                throw new Exception("No STS signing certificate defined.");
+            }
+
+//            var stsPublicKey = new X509AsymmetricSecurityKey(certificate);
+            var stsPublicKey = new X509SecurityKey(certificate);
+
+            // Check for signin endpoint over-ride
+            Uri stsEndpoint;
+            if (String.IsNullOrWhiteSpace(stsSignInEndpoint) || !Uri.TryCreate(stsSignInEndpoint, UriKind.Absolute, out stsEndpoint))
+            {
+                stsEndpoint = Utils.GetServiceEndpointFromDnsConvention(appKey: appKey);
+            }
+
+            services.AddAuthentication(sharedOptions =>
+            {
+                sharedOptions.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                sharedOptions.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                sharedOptions.DefaultChallengeScheme = WsFederationDefaults.AuthenticationScheme;
+            })
+            .AddWsFederation(options =>
+            {
+                options.Wtrealm = stsRealm;
+                options.Wreply = stsReplyTo;
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = true,
+                    ValidAudience = stsRealm,
+                    ValidateIssuer = true,
+                    ValidIssuer = stsIssuer,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = stsPublicKey
+                };
+
+                options.Configuration = new WsFederationConfiguration
+                {
+                    Issuer = stsIssuer,
+                    TokenEndpoint = new Uri(stsEndpoint, stsSignInPath).ToString(),
+                    SigningKeys =
+                    {
+                        stsPublicKey
+                    }
+                };
+
+                options.Events = new WsFederationEvents
+                {
+                    OnRedirectToIdentityProvider = notifications =>
+                    {
+                        if (notifications.ProtocolMessage.IsSignInMessage)
+                        {
+                            // Read and set additional sign in parameters
+                            string signInParameters = Configuration["dimensionData:ui:sts:signInParameters"];
+                            if (!string.IsNullOrWhiteSpace(signInParameters))
+                            {
+                                signInParameters = HttpUtility.HtmlDecode(signInParameters);
+                                Debug.Assert(signInParameters != null, "signInParameters != null");
+                                string[] parameters = signInParameters.Split('&');
+                                foreach (string param in parameters)
+                                {
+                                    string[] keyValue = param.Split('=');
+                                    if (keyValue.Length > 0)
+                                    {
+                                        notifications.ProtocolMessage.Parameters.Add(keyValue[0], keyValue[1]);
+                                    }
+                                }
+                            }
+                        }
+                        return Task.CompletedTask;
+                    },
+                    OnSecurityTokenValidated = context =>
+                    {
+                        var i = 0;
+                        return Task.CompletedTask;
+                    },
+                    OnMessageReceived = context =>
+                    {
+                        var i = 0;
+                        return Task.CompletedTask;
+                    },
+                    OnTicketReceived = context =>
+                    {
+                        var i = 0;
+                        return Task.CompletedTask;
+                    }
+                };
+            })
+            .AddCookie();
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseAuthentication();
+
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
